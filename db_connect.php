d4308e38dee59926b8ac3df4b8d0ce4b79b74673
FahrinNjh <fahrinnajah1711@gmail.com>
2024-12-26 23:16:22 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,18 @@
-<?php 
+<?php
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, "tmsfpcloud.mysql.database.azure.com", "tmsfpcloud", "Qwerty12345", "tms_db", 3306);
+$serverName = 'tmsfpcloud.mysql.database.azure.com';
+$userName = 'tmsfpcloud';
+$password = 'Qwerty12345';
+$port = 3306;
+$dbName = 'tms_db';
+
+$con = mysqli_init();
+
+if (!mysqli_real_connect($con, $serverName, $userName, $password, $dbName, $port, NULL)) {
+  
+    die("Koneksi gagal: " . mysqli_connect_error());
+echo "Koneksi berhasil!";
+
+mysqli_close($con);
+
+?>
