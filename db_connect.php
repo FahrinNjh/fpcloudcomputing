a79f8a1d800a89e64e48ce5d3fe1d7a70e0bcb17
FahrinNjh <fahrinnajah1711@gmail.com>
2024-12-26 23:04:30 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,5 @@
 <?php 
 
 $con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
 mysqli_real_connect($conn, "tmsfpcloud.mysql.database.azure.com", "tmsfpcloud", "Qwerty12345", "tms_db", 3306, MYSQLI_CLIENT_SSL);
